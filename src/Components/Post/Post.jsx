b48445bc2b8ef976a5ab7e211c26102d39af6144
MahediHasan03 <mahedihasanapon333@gmail.com>
2026-07-24 02:19:36 +0600

    navigate function added

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import '../User/User.css'
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const Post = ({post}) => {
+    const navigate = useNavigate();
 
-    const {title, userId} = post;
+    const handleNavigate = ()=> {
+        navigate('/laptops')
+    }
+
+    const {id, title, userId} = post;
 
     return (
         <div className='card'>
             <p>User ID: {userId}</p>
             <p style={{color: 'red'}}>{title}</p>
             <Link to={`/posts/${post.id}`}>
-            <button>Post Details</button>
+            <button className='button'>Post Details</button>
             </Link>
+            <button className='button' onClick={handleNavigate}>Detail of {id}</button>
         </div>
     );
 };
--- src/Components/Post/PostDetails/PostDetails.jsx
+++ src/Components/Post/PostDetails/PostDetails.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
 
+    const navigate = useNavigate()
     const post = useLoaderData();
     const {title,body} = post;
     return (
         <div style={{maxWidth:'800px'}}>
             <p>{title}</p>
             <p>{body}</p>
+            <button onClick={() => navigate(-1)}> Go Back...</button>
         </div>
     );
 };
